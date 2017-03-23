85d90235ea9c6f83ad6ce3dbf6489c4c732b037e
Daniel Pett <dpett@britishmuseum.org>
2017-03-23 10:25:40 +0000

    Fixed conflicts

--- britishMuseumFaces.py
+++ britishMuseumFaces.py
@@ -19,30 +19,6 @@ import cv2
 import argparse
 import time
 
-parser = argparse.ArgumentParser(description='A script for retrieving images from British Museum Research Space and '
-                                             'perform montage and facial recognition')
-
-parser.add_argument('-p', '--path', help='The path in which to run this script', required=True)
-# An example would be: --path '/Users/danielpett/githubProjects/scripts/'
-
-# Parse arguments
-args = parser.parse_args()
-
-# Change this to your script path
-basePath = args.path
-
-# Make the base directories
-if not os.path.exists(os.path.join(basePath, 'bmimages')):
-    os.makedirs(os.path.join(basePath, 'bmimages'))
-if not os.path.exists(os.path.join(basePath, 'bmimagesResized')):
-    os.makedirs(os.path.join(basePath, 'bmimagesResized'))
-if not os.path.exists(os.path.join(basePath, 'montages')):
-    os.makedirs(os.path.join(basePath, 'montages'))
-if not os.path.exists(os.path.join(basePath, 'facesDetected')):
-    os.makedirs(os.path.join(basePath, 'facesDetected'))
-if not os.path.exists(os.path.join(basePath, 'opencv')):
-    os.makedirs(os.path.join(basePath, 'opencv'))
-
 def make_executable(path):
     """
     Make the file executable
@@ -104,13 +80,62 @@ def resize_and_crop(img_path, modified_path, size, crop_type='top'):
                 Image.ANTIALIAS)
     img.save(modified_path)
 
+def count_files( path, extension ):
+    list_dir = []
+    list_dir = os.listdir(path)
+    count = 0
+    for fn in list_dir:
+        if fn.endswith(extension):  # eg: '.txt'
+            count += 1
+    return count
 
+def create_montage( fn ):
+    """
+    Create the montage if file exists with a try catch block
+    :param file:
+    :return:
+    """
+    if os.path.isfile(fn):
+        print("File exists")
+        try:
+            # Make sure you are in correct directory
+            # This will produce multiple tiles for large results
+            # Make sure you are in correct directory
+            make_executable(fn)
+            #time.sleep(5)
+            subprocess.call("montage -border 0 -geometry 660x -tile 10x10 @" + os.path.basename(fn) + " montages/bmPortraitBusts.jpg", shell=True)
+            # This call makes a montage of the faces detected
+            subprocess.call("montage -border 0 -geometry 660x -tile " + dims + " facesDetected/* montages/bmPortraitBustsFaces.jpg", shell=True)
+        except:
+            # The process failed
+            raise ValueError("Montage generation failed")
 
-# Set up your sparql endpoint
-sparql = SPARQLWrapper("http://collection.britishmuseum.org/sparql")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A script for retrieving images from British Museum Research Space and '
+                                             'perform montage and facial recognition')
 
-# Set your query
-sparql.setQuery("""PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    parser.add_argument('-p', '--path', help='The path in which to run this script', required=True)
+    # An example would be: --path '/Users/danielpett/githubProjects/scripts/'
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Change this to your script path
+    basePath = args.path
+
+    # Define the base directories
+    paths = {x: os.path.join(basePath, x) for x in ['bmimages', 'bmimagesResized', 'montages', 'facesDetected', 'opencv']}
+
+    # Create them if they don't already exist
+    for path in paths.values():
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    # Set up your sparql endpoint
+    sparql = SPARQLWrapper("http://collection.britishmuseum.org/sparql")
+
+    # Set your query
+    sparql.setQuery("""PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX crm: <http://erlangen-crm.org/current/>
 PREFIX fts: <http://www.ontotext.com/owlim/fts#>
 PREFIX bmo: <http://collection.britishmuseum.org/id/ontology/>
@@ -122,128 +147,100 @@ WHERE {
   ?object bmo:PX_has_main_representation ?image .
 } LIMIT 100""")
 
-# Return the JSON triples
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
+    # Return the JSON triples
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
 
-# Open the file for writing urls (this is for image magick)
-listImages = open('bmimagesResized/files.txt', 'w')
-
-
-# Iterate over the results
-for result in results["results"]["bindings"]:
-    image = result["image"]["value"]
-    if os.path.isfile(os.path.join('bmimages', os.path.basename(image))):
-        print("File already exists")
-    else:
-        path = os.path.join('bmimages', os.path.basename(image))
-        urllib.urlretrieve(image, path)
-        print("Image " + os.path.basename(image) + " downloaded")
-
-for file in os.listdir('bmimages'):
-    if not file.startswith('.'):
-        listImages.write(os.path.join("bmimagesResized", os.path.basename(file)) + "\n")
-
-# Iterate through files and crop as required
-for file in os.listdir('bmimages'):
-    # Make sure file is not a hidden one etc
-    if not file.startswith('.') and os.path.isfile(os.path.join('bmimages', file)):
-        # Open the file checking if it is valid or not. It fails otherwise :-(
-        try:
-            if not os.path.exists(os.path.join('bmimagesResized', file)):
-                resize_and_crop(os.path.join('bmimages', file), os.path.join('bmimagesResized', file), (300, 300))
-                print(file + " resized")
-            else:
-                print("Resized file exists")
-        except:
-            pass
-
-cascPath = "opencv/haarcascade_frontalface_default.xml"
-# Check you have this file, if not get it
-if not os.path.isfile(cascPath):
-    haar = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
-    urllib.urlretrieve(haar, os.path.join("opencv", os.path.basename(haar)))
-
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
-start = time.time()
-for file in os.listdir('bmimages'):
-    if not file.startswith('.'):
-        start = time.time()
-        print("Detecting faces in " + os.path.join(basePath, 'bmimages', file))
-        image = cv2.imread(os.path.join(basePath, 'bmimages', file))
-
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(150, 150),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-        left = 10
-        right = 10
-        top = 10
-        bottom = 10
-        if(len(faces) > 0):
-            for (x, y, w, h) in faces:
-                image  = image[y-top:y+h+bottom, x-left:x+w+right]
-                filename = "facesDetected/cropped_{1}_{0}".format(str(file),str(x))
-                if not os.path.exists(filename):
-                    cv2.imwrite(filename, image)
+    # Open the file for writing urls (this is for image magick)
+    listImages = open(os.path.join(paths['bmimagesResized'], "files.txt"), 'w')
 
 
-end = time.time()
-print(end - start)
+    # Iterate over the results
+    for result in results["results"]["bindings"]:
+        image = result["image"]["value"]
+        if os.path.isfile(os.path.join(paths['bmimages'], os.path.basename(image))):
+            print("File already exists")
+        else:
+            path = os.path.join(paths['bmimages'], os.path.basename(image))
+            urllib.urlretrieve(image, path)
+            print("Image " + os.path.basename(image) + " downloaded")
 
-# Iterate through files and crop as required
-for file in os.listdir('facesDetected'):
-    # Make sure file is not a hidden one etc
-    if not file.startswith('.') and os.path.isfile(os.path.join('facesDetected', file)):
-        # Open the file checking if it is valid or not. It fails otherwise :-(
-        try:
-            if not os.path.exists(os.path.join('facesDetected', file)):
-                resize_and_crop(os.path.join('facesDetected', file), os.path.join('facesDetected', file), (300, 300))
-                print(file + " resized")
-            else:
-                print("Resized file exists")
-        except:
-            pass
+    for fn in os.listdir(paths['bmimages']):
+        if not fn.startswith('.'):
+            listImages.write(os.path.join(paths["bmimagesResized"], os.path.basename(fn)) + "\n")
 
-def count_files( path, extension ):
-    list_dir = []
-    list_dir = os.listdir(path)
-    count = 0
-    for file in list_dir:
-        if file.endswith(extension):  # eg: '.txt'
-            count += 1
-    return count
+    # Iterate through files and crop as required
+    for fn in os.listdir(paths['bmimages']):
+        # Make sure file is not a hidden one etc
+        if not fn.startswith('.') and os.path.isfile(os.path.join(paths['bmimages'], fn)):
+            # Open the file checking if it is valid or not. It fails otherwise :-(
+            try:
+                if not os.path.exists(os.path.join(paths['bmimagesResized'], fn)):
+                    resize_and_crop(os.path.join(paths['bmimages'], fn), os.path.join(paths['bmimagesResized'], fn), (300, 300))
+                    print(fn + " resized")
+                else:
+                    print("Resized file exists")
+            except:
+                pass
 
-a = count_files("facesDetected", ".jpg")
-print(str(a) + " faces were identified")
-dims = "10x" + str(a/10)
-print(dims)
+    # Amended to be relevant to the base path?
+    cascPath = os.path.join(paths["opencv"], "haarcascade_frontalface_default.xml")
+    # Check you have this file, if not get it
+    if not os.path.isfile(cascPath):
+        haar = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
+        urllib.urlretrieve(haar, cascPath)
 
-def create_montage( file ):
-    """
-    Create the montage if file exists with a try catch block
-    :param file:
-    :return:
-    """
-    if os.path.isfile(file):
-        print("File exists")
-        try:
-            # Make sure you are in correct directory
-            # This will produce multiple tiles for large results
-            # Make sure you are in correct directory
-            make_executable(file)
-            #time.sleep(5)
-            subprocess.call("montage -border 0 -geometry 660x -tile 10x10 @" + os.path.basename(file) + " montages/bmPortraitBusts.jpg", shell=True)
-            # This call makes a montage of the faces detected
-            subprocess.call("montage -border 0 -geometry 660x -tile " + dims + " facesDetected/* montages/bmPortraitBustsFaces.jpg", shell=True)
-        except:
-            # The process failed
-            raise ValueError("Montage generation failed")
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    start = time.time()
+    for fn in os.listdir(paths['bmimages']):
+        if not fn.startswith('.'):
+            start = time.time()
+            print("Detecting faces in " + os.path.join(paths['bmimages'], fn))
+            image = cv2.imread(os.path.join(paths['bmimages'], fn))
 
-create_montage("files.txt")
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(150, 150),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            left = 10
+            right = 10
+            top = 10
+            bottom = 10
+            if(len(faces) > 0):
+                for (x, y, w, h) in faces:
+                    image  = image[y-top:y+h+bottom, x-left:x+w+right]
+                    filename = os.path.join(paths["facesDetected"], "cropped_{1}_{0}".format(str(fn),str(x)))
+                    if not os.path.exists(filename):
+                        cv2.imwrite(filename, image)
+
+
+    end = time.time()
+    print(end - start)
+
+    # Iterate through files and crop as required
+    for fn in os.listdir(paths['facesDetected']):
+        # Make sure file is not a hidden one etc
+        if not fn.startswith('.') and os.path.isfile(os.path.join(paths["facesDetected"], fn)):
+            # Open the file checking if it is valid or not. It fails otherwise :-(
+            try:
+                if not os.path.exists(os.path.join(paths["facesDetected"], fn)):
+                    resize_and_crop(os.path.join(paths["facesDetected"], fn), os.path.join(paths["facesDetected"], fn), (300, 300))
+                    print(fn + " resized")
+                else:
+                    print("Resized file exists")
+            except:
+                pass
+
+
+    a = count_files(paths["facesDetected"], ".jpg")
+    print(str(a) + " faces were identified")
+    dims = "10x" + str(a/10)
+    print(dims)
+
+    create_montage("files.txt")
